8abd33b0ef15552a23f82d028ed54c547293c985
matheusDev <matheus.info29@gmail.com>
2024-03-09 19:00:12 -0300

    Update

--- lib/components/drawerhome_screen.dart
+++ lib/components/drawerhome_screen.dart
@@ -11,17 +11,37 @@ class DrawerHome extends StatefulWidget {
 class _DrawerHomeState extends State<DrawerHome> {
   @override
   Widget build(BuildContext context) {
-    return const Drawer(
-      child: DrawerHeader(
-          child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Wrap(
-            children: [Icon(FontAwesomeIcons.solidUser), Text("data")],
-          ),
-        ],
-      )),
-    );
+    return Drawer(
+        child: ListView(
+      children: const [
+        DrawerHeader(
+            child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Wrap(
+              children: [
+                Icon(FontAwesomeIcons.solidUser),
+                Text("Matheus Santana")
+              ],
+            ),
+          ],
+        )),
+        ListTile(
+          title: Text('Serviços'),
+        ),
+        ListTile(
+          title: Text('Minha conta'),
+        ),
+        ListTile(
+          title: Text('Cartões'),
+        ),
+        ListTile(
+          title: Text('Auto atendimento'),
+        ),
+        ListTile(
+          title: Text('Sair'),
+        ),
+      ],
+    ));
   }
 }
