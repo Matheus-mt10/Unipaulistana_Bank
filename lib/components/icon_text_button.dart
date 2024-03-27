ce1f240eb761aeb59f9302bc7dfcde3756a6a308
matheusDev <matheus.info29@gmail.com>
2024-03-27 19:48:27 -0300

    Update

---
+++ lib/components/icon_text_button.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class IconTextButton extends StatelessWidget {
+  const IconTextButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  Column(
+      children: [
+        Container(
+          color: Color(Colors.red),
+        )
+      
+      ],
+    );
+  }
+}
